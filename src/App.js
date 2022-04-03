0d5389113b22773a1e834619aa43f18b5003e9fc
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-03 17:34:44 +0900

    :memo: #2.3 | 'as' and Attrs
    - attrs를 이용해서 tag 기능 확장

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Btn = styled.button`
-  color: white;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
   background-color: tomato;
-  border: 0;
-  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father as="header">
-      <Btn>Log in</Btn>
-      <Btn as="a" href="#">
-        Log in
-      </Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
