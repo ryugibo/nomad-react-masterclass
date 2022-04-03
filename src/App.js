5ad43ad8dfd36520ef159acdb7b821986fd0d888
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-03 17:31:59 +0900

    :memo: #2.3 | 'as' and Attrs
    - as를 이용해서 태그를 덮어씌우기

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50%;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 50px;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Btn>Log in</Btn>
+      <Btn as="a" href="#">
+        Log in
+      </Btn>
     </Father>
   );
 }
