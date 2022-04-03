0dec08e2170728286fe3d80a75e05942974819b3
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-03 18:10:34 +0900

    :memo: #2.7 | Themes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 const Wrapper = styled.div`
   display: flex;
-`;
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-  }
-  ${Emoji}:active {
-    opacity: 0;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
-      <Emoji>😂</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
