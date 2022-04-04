235056c82d6f28d9a05b13e7206c4eee56ad52db
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-04 21:34:53 +0900

    :memo: #3.3 | Optional Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,18 +1,32 @@
 import styled from "styled-components";
+import { Tracing } from "trace_events";
 
-interface CircleProps {
+interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
-const Container = styled.div<CircleProps>`
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 50%;
+  border: 3px solid ${(props) => props.borderColor};
 `;
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  // boraderColor: string | undefined; 로도 작성가능
+  text?: string;
+}
+
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
