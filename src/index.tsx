5ecb8a76b3517b5b18455508e39a845ff7115d4d
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-07 09:47:25 +0900

    :memo: #4.0 | Setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme, lightTheme } from "./theme";
+import { defaultTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={defaultTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
