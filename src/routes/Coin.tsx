d4bb5ad4f4d1b3dd48fc1c768340228165375350
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-08 13:14:30 +0900

    :memo: #4.5 | Coin Data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -33,11 +33,30 @@ interface RouteState {
   name: string;
 }
 
+// https://api.coinpaprika.com/v1/coins/btc-bitcoin
+// https://api.coinpaprika.com/v1/tickers/btc-bitcoin
+
 function Coin() {
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
 
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
