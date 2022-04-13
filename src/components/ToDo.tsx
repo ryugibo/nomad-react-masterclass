f61565140f78061fd62c6d71384804b83e8b7575
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-13 21:31:09 +0900

    :memo: #5.13 | Categories
    - 익명 함수를 사용하는 방식

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,21 @@
 import { IToDo } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category }: IToDo) {
+  const onClick = (newCategory: IToDo["category"]) => {
+    console.log(newCategory);
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Done</button>
-      <button>Doing</button>
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
