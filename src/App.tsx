0e178c6f431ca8e6787e20b3468da6ed92faf2d5
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-16 04:09:40 +0900

    :memo: #7.8 | MotionValues part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
@@ -35,15 +35,15 @@ const circleVariants = {};
 
 function App() {
   const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0]);
   useEffect(() => {
-    x.onChange(() => {
-      console.log(x.get());
+    scale.onChange(() => {
+      console.log(scale.get());
     });
-  }, [x]);
+  }, [scale]);
   return (
     <Wrapper>
-      <button onClick={() => x.set(200)}>Click me</button>
-      <Box style={{ x }} drag="x" dragSnapToOrigin />
+      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
