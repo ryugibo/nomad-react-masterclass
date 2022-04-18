83bfda1d32f72727f7e781ae8f18d25841224e9c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-04-18 12:26:36 +0900

    :memo: #8.6 | Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "3c983d1e60d94e03820760af3fae791e";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(
     `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=ko-KR`
